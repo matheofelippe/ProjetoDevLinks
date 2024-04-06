5c3a7c8b2e7af0b49f9f224d0d030c42427a2a45
Felippe Matheo <matheofelippe@gmail.com>
2024-04-06 19:14:10 -0300

    adicionado light mode, js e finalizacao

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+    const hmtl = document.documentElement
+    hmtl.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+
+    if(hmtl.classList.contains('light')){
+        img.setAttribute("src", "./assets/avatar-light.png")
+    }else{
+        img.setAttribute("src", "./assets/avatar.png")
+    }
+
+}
